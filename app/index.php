7062f2423c973f3ff5693b61690b3bc15c24107c
Carbocode <noreply@prisco.me>
2024-05-19 12:05:43 +0200

    fixes

--- app/index.php
+++ app/index.php
@@ -1,5 +1,5 @@
 <?php
-include '../constants.php';
+include './constants.php';
 
 if (!defined('DB_HOST') || !defined('DB_USER') || !defined('DB_PSWD') || !defined('DB_NAME')) {
     die("Errore: costanti di connessione non definite correttamente. Controlla che nella cartella app/ sia presente il file constants.php");
@@ -11,19 +11,19 @@ if (!defined('DB_HOST') || !defined('DB_USER') || !defined('DB_PSWD') || !define
 
 <head>
     <title>Pizzeria</title>
-    <link rel="stylesheet" href="../global.css">
+    <link rel="stylesheet" href="/app/global.css">
 
-    <link rel="apple-touch-icon" sizes="180x180" href="../assets/icons/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="../assets/icons/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="../assets/icons/favicon-16x16.png">
-    <link rel="manifest" href="../assets/icons/site.webmanifest">
-    <link rel="mask-icon" href="../assets/icons/safari-pinned-tab.svg" color="#5bbad5">
-    <meta name="msapplication-TileColor" content="#ffc40d">
+    <link rel="apple-touch-icon" sizes="180x180" href="app/assets/icons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="app/assets/icons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="app/assets/icons/favicon-16x16.png">
+    <link rel="manifest" href="app/assets/icons/site.webmanifest">
+    <link rel="mask-icon" href="app/assets/icons/safari-pinned-tab.svg" color="#5bbad5">
+    <link rel="shortcut icon" href="app/assets/icons/favicon.ico">
     <meta name="theme-color" content="#ffffff">
 </head>
 
 <body>
-    <?php include '../components/navbar.php'; ?>
+    <?php include 'components/navbar.php'; ?>
 
     <?php
     // Ottieni il percorso richiesto
@@ -36,7 +36,7 @@ if (!defined('DB_HOST') || !defined('DB_USER') || !defined('DB_PSWD') || !define
     $request = trim($request, '/');
 
     // Determina il percorso del file
-    $filepath = __DIR__ . '/' . $request . '/page.php';
+    $filepath = __DIR__ . '/src/' . $request . '/page.php';
 
     // Verifica se il file esiste e includilo, altrimenti mostra un errore 404
     if (file_exists($filepath)) {
